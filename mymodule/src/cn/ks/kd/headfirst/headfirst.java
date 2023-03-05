0583bc546b706c3950a451d83080d1a44a145bf4
dell <wangjun0054@163.com>
2023-03-05 14:05:00 +0800

    java is rule

--- mymodule/src/cn/ks/kd/headfirst/headfirst.java
+++ mymodule/src/cn/ks/kd/headfirst/headfirst.java
@@ -3,8 +3,8 @@ package cn.ks.kd.headfirst;
 public class headfirst {
     public static void main(String[] args) {
         System.out.println("head first java");
-        System.out.println("head first java");
-        System.out.println("head first java");
+        System.out.println("hello world");
+        System.out.println("java is runle");
         System.out.println("head first java");
         System.out.println("head first java");
         System.out.println("head first java");
