2f931f5b93830eb6766750c88d2341aa729ef61e
dell <wangjun0054@163.com>
2023-03-05 16:54:43 +0800

    study 1

--- mymodule/src/cn/ks/kd/headfirst/headfirst.java
+++ mymodule/src/cn/ks/kd/headfirst/headfirst.java
@@ -3,25 +3,12 @@ package cn.ks.kd.headfirst;
 public class headfirst {
     public static void main(String[] args) {
         System.out.println("head first java");
-        System.out.println("hello world");
-        System.out.println("java is runle");
-        System.out.println("head first java");
-        System.out.println("head first java");
-        System.out.println("head first java");
-
-        System.out.println("head first java");
-
-        System.out.println("head first java");        System.out.println("head first java");
-
-        System.out.println("head first java");
-        System.out.println("head first java");
-
-        System.out.println("head first java");
-
-        System.out.println("head first java");
-
-        System.out.println("head first java");
-
+        System.out.println("java rule");
+        System.out.println("world");
+        int i = 3;
+        String x = "Dirk";
+        i = i * 17;
+        System.out.println(x + ": " + i);
 
     }
 }
