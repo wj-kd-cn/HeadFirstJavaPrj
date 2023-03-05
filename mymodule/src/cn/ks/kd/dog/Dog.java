c54ecc7ef9ddecf38a17013cdcb88784dc80d877
dell <wangjun0054@163.com>
2023-03-05 21:31:38 +0800

    Dog

---
+++ mymodule/src/cn/ks/kd/dog/Dog.java
@@ -0,0 +1,11 @@
+package cn.ks.kd.dog;
+
+public class Dog {
+    String name;
+    int size;
+    String breed;
+
+    void bark(){
+        System.out.print("Ruff Ruff !!!!");
+    }
+}
---
+++ mymodule/src/cn/ks/kd/dog/DogTestDrive.java
@@ -0,0 +1,9 @@
+package cn.ks.kd.dog;
+
+public class DogTestDrive {
+    public static void main(String[] args) {
+        Dog d1 = new Dog();
+        d1.size = 40;
+        d1.bark();
+    }
+}
