6669572c2be327b54d1650525af5be911cbf78c0
dell <wangjun0054@163.com>
2023-03-26 20:02:09 +0800

    animal and tiger and cat and sheep

--- mymodule/src/cn/ks/kd/animal/Dog.java
+++ mymodule/src/cn/ks/kd/animal/Dog.java
@@ -4,5 +4,7 @@ public class Dog extends Canine{
     public static void main(String[] args) {
         Dog adg = new Dog();
         adg.makeNoise();
+        adg.setFood("bone");
+        adg.eat();
     }
 }
