a6e30d8cabce4b525f944ca0dc5c495689d76e86
dell <wangjun0054@163.com>
2023-04-16 20:01:17 +0800

    duck

--- mymodule/src/cn/ks/kd/headfirst/Duck.java
+++ mymodule/src/cn/ks/kd/headfirst/Duck.java
@@ -2,18 +2,24 @@ package cn.ks.kd.headfirst;
 
 public class Duck {
     int size;
-
+    private static int duckCount;
     public Duck(){
+        duckCount++;
         System.out.println("create a Duck");
     }
     public Duck(int vsize){
         size = vsize;
+        duckCount++;
         System.out.println("create a Duck and size: " + Integer.toString(size));
     }
     private Duck(String msg,int vsize){
         size = vsize;
+        duckCount++;
         System.out.println("create a Duck and size: " + Integer.toString(size));
         System.out.println("Duck said: " + msg);
 
+    }
+    public int getDuckCount(){
+        return duckCount;
     }
 }
--- mymodule/src/cn/ks/kd/headfirst/TestDuck.java
+++ mymodule/src/cn/ks/kd/headfirst/TestDuck.java
@@ -3,7 +3,9 @@ package cn.ks.kd.headfirst;
 public class TestDuck {
     public static void main(String[] args) {
         Duck aduck  = new Duck(123);
+        System.out.println(aduck.getDuckCount());
         Duck bduck  = new Duck();
+        System.out.print(bduck.getDuckCount());
 
     }
 
