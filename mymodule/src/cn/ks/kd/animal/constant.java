b5575ee728f2c8764bcffa3a17c835c64bb380f6
dell <wangjun0054@163.com>
2023-04-19 21:39:43 +0800

    duck

---
+++ mymodule/src/cn/ks/kd/animal/constant.java
@@ -0,0 +1,5 @@
+package cn.ks.kd.animal;
+
+public class constant {
+    public static final double PI = 3.141592653589793;
+}
--- mymodule/src/cn/ks/kd/headfirst/TestDuck.java
+++ mymodule/src/cn/ks/kd/headfirst/TestDuck.java
@@ -1,12 +1,24 @@
 package cn.ks.kd.headfirst;
 
+import cn.ks.kd.animal.constant;
+
+import java.util.ArrayList;
+
 public class TestDuck {
     public static void main(String[] args) {
         Duck aduck  = new Duck(123);
         System.out.println(aduck.getDuckCount());
         Duck bduck  = new Duck();
         System.out.print(bduck.getDuckCount());
-
+        double x = 12.4 * constant.PI;
+        System.out.println(x);
+        TestDuck td = new TestDuck();
+        td.doNmuberNewWay();
+    }
+    public void doNmuberNewWay(){
+        ArrayList<Integer> numList = new ArrayList<Integer>();
+        numList.add(3);
+        System.out.println(numList.get(0));
     }
 
 }
