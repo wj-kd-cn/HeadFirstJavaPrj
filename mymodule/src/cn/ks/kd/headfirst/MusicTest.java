3969d180adaba2b390c20a4fc8b56ea4eca525a0
dell <wangjun0054@163.com>
2023-04-25 22:12:56 +0800

    MusicTest

---
+++ mymodule/src/cn/ks/kd/headfirst/MusicTest.java
@@ -0,0 +1,25 @@
+package cn.ks.kd.headfirst;
+
+import javax.sound.midi.MidiSystem;
+import javax.sound.midi.MidiUnavailableException;
+import javax.sound.midi.Sequencer;
+
+import static java.lang.System.out;
+public class MusicTest {
+    public void play(){
+        try {
+            Sequencer seq = MidiSystem.getSequencer();
+        }catch (MidiUnavailableException ex) {
+            out.println(ex.toString());
+
+        }
+        out.println("we had a sequencer");
+
+    }
+    public static void main(String[] args) {
+        MusicTest mt = new MusicTest();
+        mt.play();
+
+
+    }
+}
